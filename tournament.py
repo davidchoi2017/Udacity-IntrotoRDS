d33b8c798a7fbd6bc4d475edcf8d38b0b73dc3cd
David Choi <davidchoi2017@users.noreply.github.com>
2017-06-29 14:36:57 -0400

    Revisions re: Udacity 2nd project review
    These are revisions based on feedback from Udacity's second project review.
    I added variables to reduce the repetitive coding, and corrected syntax involving the order of close connection commands and return commands.
    I also added programming comments to make the code more readable.

--- tournament.py
+++ tournament.py
@@ -1,55 +1,62 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
+"""
+Coder Name: David Choi
+Date: 6-29-2017
+Purpose of this file: to retrieve data and analyze it from the "tournament" database, which is created by tournament.sql file.
+It is designed to perform various functions such as retrieve the number of players in the Swiss-style tournament, the number of matches player, the results of those matches, the win records of each player, and the matchups for the nextround in the tournament.
+"""
 
 import psycopg2
 
-
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
-
+    db = psycopg2.connect("dbname=tournament")
+    c = db.cursor()
+    return db, c
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    c.execute("delete from matches;")
-    conn.commit()
-    conn.close()
+    db, c = connect()
+    query = "truncate only matches cascade;"
+    c.execute(query)
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    c.execute("delete from players;")
-    conn.commit()
-    conn.close()
+    db, c = connect()
+    query = "truncate only players cascade;"
+    c.execute(query)
+    db.commit()
+    db.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    c.execute("select count(*) as num from players;")
+    db, c = connect()
+    query = "select count(*) as num from players;"
+    c.execute(query)
     results = c.fetchone()
+    db.close()
     return results[0]
-    conn.close()
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    c.execute("INSERT INTO players (NAME) VALUES (%s);", (name,))
-    conn.commit()
-    conn.close()
+    db, c = connect()
+    query = "INSERT INTO players (NAME) VALUES (%s);"
+    param = (name,)
+    c.execute(query, param)
+    db.commit()
+    db.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -64,13 +71,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    c.execute("SELECT * FROM winners;")
+    db, c = connect()
+    query = "SELECT * FROM winners;"
+    c.execute(query)
     results = c.fetchall()
+    db.close()
     return results
-    conn.close()
-
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -79,20 +85,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    c.execute("INSERT INTO matches (WINNER, LOSER) VALUES (%s, %s);", (winner, loser,))
-    conn.commit()
-    conn.close()
- 
+    db, c = connect()
+    query = "INSERT INTO matches (WINNER, LOSER) VALUES (%s, %s);"
+    param = (winner, loser,)
+    c.execute(query, param)
+    db.commit()
+    db.close()
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -100,9 +107,9 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    conn = psycopg2.connect("dbname=tournament")
-    c = conn.cursor()
-    c.execute("SELECT * FROM nextround;")
+    db, c = connect()
+    query = "SELECT * FROM nextround;"
+    c.execute(query)
     results = c.fetchall()
+    db.close()
     return results
-    conn.close()
